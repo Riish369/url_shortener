094f6b8807f13be55caa9629e7ea718fa7a675aa
Rishank <rishank.sharma.1426@gmail.com>
2026-04-10 03:40:29 +0530

    added object model entity

---
+++ src/main/java/com/example/URL_Shortener/entity/Url.java
@@ -0,0 +1,34 @@
+package com.example.URL_Shortener.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable=false, length=2048)
+    private String originalUrl;
+
+    @Column(nullable=false, length=20)
+    private String shortUrl;
+
+    @Column(nullable=false)
+    private LocalDateTime dateTime;
+
+    @Builder.Default
+    @Column(nullable=false)
+    private Long accessCount = 0L;
+
+
+}
